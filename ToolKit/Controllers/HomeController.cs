2935e2b6c87889297e7df1bd424a071c0b975ecf
pragya sharma <pragyasharma.techie@gmail.com>
2026-03-12 21:00:16 +0530

    Add project files.

---
+++ ToolKit/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ToolKit.Models;
+
+namespace ToolKit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Age()
+        {
+            return View();
+        }
+
+        public IActionResult Unit()
+        {
+            return View();
+        }
+
+        public IActionResult Password()
+        {
+            return View();
+        }
+
+        public IActionResult RandomQuote() 
+        { 
+        
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ToolKit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToolKit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
